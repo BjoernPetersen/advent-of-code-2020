f9f14aa3cde71e070b2a244a9a011b5c42044918
Björn Petersen <pheasn@gmail.com>
2020-12-05 14:55:13 +0100

    Change day1 struct

--- src/day01.rs
+++ src/day01.rs
@@ -1,22 +1,22 @@
 use crate::day::Day;
 use crate::input::parse_ints;
 
-// foreach-loops go brrrrrrrrrrrrrrrrrrrrr
 pub struct Day1 {
-    input: String,
+    input: Vec<i32>,
 }
 
 impl Day1 {
     pub fn new(input: String) -> Self {
-        Day1 { input }
+        Day1 { input: parse_ints(&input).collect() }
     }
 }
 
+// foreach-loops go brrrrrrrrrrrrrrrrrrrrr
 impl Day for Day1 {
     fn task1(&self) -> String {
-        let numbers: Vec<_> = parse_ints(&self.input).collect();
-        for n in &numbers {
-            for m in &numbers {
+        let numbers = &self.input;
+        for n in numbers {
+            for m in numbers {
                 if n + m == 2020 {
                     return format!("{}", n * m);
                 }
@@ -26,10 +26,10 @@ impl Day for Day1 {
     }
 
     fn task2(&self) -> String {
-        let numbers: Vec<_> = parse_ints(&self.input).collect();
-        for n in &numbers {
-            for m in &numbers {
-                for o in &numbers {
+        let numbers = &self.input;
+        for n in numbers {
+            for m in numbers {
+                for o in numbers {
                     if n + m + o == 2020 {
                         return format!("{}", n * m * o);
                     }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate advent20;
 
 fn main() {
     // TODO accept cli arg
-    let day = advent20::get_day(2);
+    let day = advent20::get_day(1);
     println!("Result 1: {}", day.task1());
     println!("Result 2: {}", day.task2());
 }
